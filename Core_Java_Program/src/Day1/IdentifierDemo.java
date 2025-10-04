77bcaf0e58efe024644c1ed798fb795bb6580248
kalaiselvan-0108 <kalaiselvan0q@gmail.com>
2025-10-04 20:29:07 +0530

    JavaProgram's

---
+++ Core_Java_Program/src/Day1/IdentifierDemo.java
@@ -0,0 +1,16 @@
+package Day1;
+
+public class IdentifierDemo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int $number = 20; 
+		System.out.println("value of the number variable is : "+ $number);
+	
+		String studentName="Aniket"; 
+		System.out.println("value is : "+ studentName);
+	}
+
+	}
+
+
---
+++ Core_Java_Program/src/day_2/ContinueDemo.java
@@ -0,0 +1,17 @@
+package day_2;
+
+public class ContinueDemo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		for (int k = 5; k < 15; k++)
+		{
+	
+		if (k%2 != 0)
+		continue;
+	
+		System.out.print(k + " ");
+		}
+	}
+
+}
